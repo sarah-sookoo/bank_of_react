8e95188ab7c26a3c9d82dc5a95091d46a6bdcb07
sarahsookoo <sarah_sookoo@outlook.com>
2022-11-09 14:35:55 -0500

    implemented Debits

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -4,11 +4,11 @@
 // The Debits component contains information for Debits page view.
 // Note: You need to work on this file for the Assignment.
 // ==================================================*/
-// import {Link} from 'react-router-dom';
 
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
 import AccountBalance from './AccountBalance';
+import List from './List';
 
 class Debit extends Component {
   constructor(props) {
@@ -38,29 +38,26 @@ class Debit extends Component {
     })
   }
 
- render() {
+  render() {
     return (
       <div>
         <h1> Debits </h1>
         <h3><AccountBalance accountBalance={this.state.accountBalance} /></h3>
-        
-          <label htmlFor="description">Enter Debit: </label>
-          <input type="text" id="description" name="description"></input><br />
-          <label htmlFor="amount">Amount: </label>
-          <input type="text" id="amount" name="amount"></input><br />
-          <button onClick={this.newDebit}>Submit</button>
+
+        <label htmlFor="description">Enter Debit: </label>
+        <input type="text" id="description" name="description"></input><br />
+        <label htmlFor="amount">Amount: </label>
+        <input type="text" id="amount" name="amount"></input><br />
+        <button onClick={this.newDebit}>Submit</button>
         <br></br>
 
-        {/* {this.state.debits.map((list, index) => (
-                    <list 
-                        key={index} 
-                        description={list.description}
+        {this.state.debits.map((list) => (
+                    <List 
                         amount={list.amount}
+                        description={list.description}
                         date={list.date}
-                        id={list.id}
-                        
                     />
-                ))} */}
+                ))}
 
         <Link to="/">Home Page</Link> <br />
         <Link to="/userProfile">Use Profile</Link>
